7c02923f591ff5899a025a71ea536644b9e24f96
Emmanuel <rojaz813@gmail.com>
2021-01-02 16:16:06 +0300

    Initial commit  🏁

---
+++ nuxt.config.js
@@ -0,0 +1,31 @@
+export default {
+  target: 'static',
+
+  head: {
+    title: 'Norman Nuthu',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Norman Nuthu Portfolio'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  css: [],
+
+  components: true,
+
+  buildModules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss'],
+
+  modules: ['@nuxtjs/axios', '@nuxtjs/pwa', '@nuxt/content'],
+
+  axios: {},
+
+  content: {},
+
+  build: {}
+};
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
